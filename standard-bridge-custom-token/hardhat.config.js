34925693806ea722aa42db521e557f7e9e7382bd
Eskrano <pommmper@gmail.com>
2023-02-28 23:21:08 +0200

    Updates flow

--- standard-bridge-custom-token/hardhat.config.js
+++ standard-bridge-custom-token/hardhat.config.js
@@ -4,24 +4,24 @@ require('@nomiclabs/hardhat-ethers')
 // Load environment variables from .env
 require('dotenv').config();
 
-const words = process.env.MNEMONIC.match(/[a-zA-Z]+/g).length
-validLength = [12, 15, 18, 24]
-if (!validLength.includes(words)) {
-  console.log(`The mnemonic (${process.env.MNEMONIC}) is the wrong number of words`)
-  process.exit(-1)
-}
+// const words = process.env.MNEMONIC.match(/[a-zA-Z]+/g).length
+// validLength = [12, 15, 18, 24]
+// if (!validLength.includes(words)) {
+//   console.log(`The mnemonic (${process.env.MNEMONIC}) is the wrong number of words`)
+//   process.exit(-1)
+// }
 
 module.exports = {
   networks: {
     'tanenbaum': {
       chainId: 5700,
       url: `https://rpc.tanenbaum.io`,
-      accounts: { mnemonic: process.env.MNEMONIC }
+      accounts: [process.env.PK]
     },
-    'optimism-mainnet': {
-      chainId: 10,
+    'rollux': {
+      chainId: 57000,
       url: `https://rpc-bedrock.rollux.com/`,
-      accounts: { mnemonic: process.env.MNEMONIC }
+      accounts: [process.env.PK]
     }
   },
   solidity: '0.8.13',
