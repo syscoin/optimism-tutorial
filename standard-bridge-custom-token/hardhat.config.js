724bad02b0ec42d55f24ff9a64b043c278e908c1
Eskrano <pommmper@gmail.com>
2023-02-28 13:53:04 +0200

    Added rollux/tanenbaum networks

--- standard-bridge-custom-token/hardhat.config.js
+++ standard-bridge-custom-token/hardhat.config.js
@@ -7,20 +7,20 @@ require('dotenv').config();
 const words = process.env.MNEMONIC.match(/[a-zA-Z]+/g).length
 validLength = [12, 15, 18, 24]
 if (!validLength.includes(words)) {
-   console.log(`The mnemonic (${process.env.MNEMONIC}) is the wrong number of words`)
-   process.exit(-1)
+  console.log(`The mnemonic (${process.env.MNEMONIC}) is the wrong number of words`)
+  process.exit(-1)
 }
 
 module.exports = {
   networks: {
-    'optimism-goerli': {
-      chainId: 420,
-      url: `https://opt-goerli.g.alchemy.com/v2/${process.env.L2_ALCHEMY_KEY}`,
+    'tanenbaum': {
+      chainId: 5700,
+      url: `https://rpc.tanenbaum.io`,
       accounts: { mnemonic: process.env.MNEMONIC }
     },
     'optimism-mainnet': {
       chainId: 10,
-      url: `https://opt-mainnet.g.alchemy.com/v2/${process.env.L2_ALCHEMY_KEY}`,
+      url: `https://rpc-bedrock.rollux.com/`,
       accounts: { mnemonic: process.env.MNEMONIC }
     }
   },
